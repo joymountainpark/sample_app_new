37728da5705917b979fdcf1d89047da87f2bc899
Daniel Park <kainos@localhost.localdomain>
2015-07-19 13:07:44 +0900

    Added Edit Profile Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update]
 
-	def show
+	  def show
 		@user = User.find(params[:id])
-	end
+	  end
 
   	def new
   		@user = User.new
@@ -19,10 +20,36 @@ class UsersController < ApplicationController
   		end
   	end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end 
+
   	private
 
   		def user_params
   			params.require(:user).permit(:name, :email, :password, 
   				                          :password_confirmation)
   		end
+
+      # Before filters
+
+
+      # Confirms a logged-in user.
+      def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+
+      end
 end
